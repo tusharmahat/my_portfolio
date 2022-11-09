bb5e3218c0c2a26885be4d433fa9225a284b05b1
Tushar Mahat <tusharmahat@gmail.com>
2022-11-09 19:54:55 -0400

    home 'view proect btn' fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,14 +12,17 @@ const Home = () => {
                 <h2 className="text-4xl sm:text-7xl font-bold text-[#d9d9d997]">I am a Full Stack Developer</h2>
                 <p className="text-[#d9d9d997] py-4 max-w-[700px]">I am a full-stack developer focused on building responsive full-stack web application</p>
                 <div >
-                    <button className="text-white group border-2 rounded-md px-6 py-3 my-2 flex items-center hover:bg-[#e63946] hover:border-[#e63946]">
-                        <Link to="projects" smooth={true} duration={500} >
+
+                    <Link to="projects" smooth={true} duration={500} >
+                        <button className="text-white group border-2 rounded-md px-6 py-3 my-2 flex items-center hover:bg-[#e63946] hover:border-[#e63946]">
                             View Projects
-                        </Link>
-                        <span className="group-hover:rotate-90 duration-300">
-                            <HiArrowNarrowRight className="ml-3" />
-                        </span>
-                    </button>
+                            <span className="group-hover:rotate-90 duration-300">
+                                <HiArrowNarrowRight className="ml-3" />
+                            </span>
+                        </button>
+                    </Link>
+
+
                 </div>
             </div>
         </div >
